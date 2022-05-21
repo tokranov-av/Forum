76e4d901e9f3322a1867a0944e998449c24a4ba2
Артур Токранов <artur.tokranov@gmil.com>
2022-05-21 17:43:29 +0300

    Добавил описание, докер-файлы, подкорректировал урлы

--- forum/forum/urls.py
+++ forum/forum/urls.py
@@ -6,8 +6,11 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/', include('app_forum.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(),
+         name='token_verify'),
+    path('api/v1/', include('app_forum.urls')),
 ]
